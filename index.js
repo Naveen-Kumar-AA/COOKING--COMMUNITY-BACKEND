d443af08269fda435d71612c10c23f8c8cf5c5c6
Naveen-Kumar-AA <naveen94865@gmail.com>
2022-05-30 23:18:47 +0530

    init_branch_commit1

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const Joi = require('joi');
 const app = express();
+const cors = require('cors');
+const db = require('./oracle.js')
 
+app.use(cors())
 app.use(express.json());
 
 const courses = [
-    { id: 1, name: 'course1'},
-    { id: 2, name: 'course2'},
-    { id: 3, name: 'course3'},
-    { id: 4, name: 'course4'},
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+    { id: 4, name: 'course4' },
 ];
 
-app.get('/', (request,response) => {
+app.get('/', (request, response) => {
     response.send('Hello world');
 });
 
@@ -21,12 +24,20 @@ app.get('/', (request,response) => {
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send('The course with the given id was not found');//404 - object not found
+    if (!course) return res.status(404).send('The course with the given id was not found');//404 - object not found
     res.send(course);
 });
 
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
+app.get('/user-passwd', (req, res) => {
+    // res.send(courses);
+    // console.log(db.selectAllUsers())
+    // res.send(db.selectAllUsers())
+    db.selectAllUsers().then((result) => {
+        res.send(result)
+    }).catch((err) => {
+        console.log(err);
+    });
+
 });
 
 
@@ -34,8 +45,8 @@ app.post('/api/courses', (req, res) => {
 
     const { error } = validateCourse(req.body);     //eq to result.error
     // if invalid, return 400 - Bad request
-    if(error) return res.status(400).send(error.details[0].message);
-    
+    if (error) return res.status(400).send(error.details[0].message);
+
 
     // const schema = {
     //     name : Joi.string().min(3).required()
@@ -58,11 +69,34 @@ app.post('/api/courses', (req, res) => {
     res.send(course);
 });
 
-app.put('/api/courses/:id', (req,res) =>{
+app.post('/check-user-passwd', (req, res) => {
+
+    const { error } = validateUserPasswd(req.body);     //eq to result.error
+    // if invalid, return 400 - Bad request
+    if (error) return res.status(400).send(error.details[0].message);
+
+
+    // const course = {
+    //     id: courses.length + 1,
+    //     name: req.body.name
+    // };
+
+    // res.send(db.checkUserPassword(req.body.username, req.body.password))
+
+    db.checkUserPassword(req.body.username, req.body.password).then((result) => {
+        res.send(result)
+    }).catch((err) => {
+        console.log(err);
+    });
+    // res.send(row)
+});
+
+
+app.put('/api/courses/:id', (req, res) => {
     // look up the course
     // if not existing, return 404
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send('The course with the given id was not found');//404 - object not found
+    if (!course) return res.status(404).send('The course with the given id was not found');//404 - object not found
 
 
 
@@ -76,12 +110,12 @@ app.put('/api/courses/:id', (req,res) =>{
     // const result = validateCourse(req.body);
     const { error } = validateCourse(req.body);     //eq to result.error
     // if invalid, return 400 - Bad request
-    if(error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
 
 
     // update course
     course.name = req.body.name;
-       
+
     // return the updated course
     res.send(course);
 
@@ -90,22 +124,32 @@ app.put('/api/courses/:id', (req,res) =>{
 
 function validateCourse(course) {
     const schema = {                                  // Put this into a
-        name : Joi.string().min(3).required()         // function to  
+        name: Joi.string().min(3).required()         // function to  
     };                                                // reuse
 
     return Joi.validate(course, schema);
 }
 
+function validateUserPasswd(user_passwd) {
+    const schema = {                                    
+        username: Joi.string().required(),
+        password: Joi.string().required()
+    };  
+
+    return Joi.validate(user_passwd, schema);
+}
+
+
 app.delete('/api/courses/:id', (req, res) => {
     // Look up for the course
     // Not existing, return 404
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send('The course with the given id was not found');//404 - object not found
+    if (!course) return res.status(404).send('The course with the given id was not found');//404 - object not found
 
 
     // Delete
     const index = courses.indexOf(course);
-    courses.splice(index,1);    //Go to the index and remove one object
+    courses.splice(index, 1);    //Go to the index and remove one object
 
 
 
--- oracle.js
+++ oracle.js
@@ -1,35 +1,95 @@
 // import res from "express/lib/response";
-const oracledb = require('oracledb')
+const oracledb = require('oracledb');
+const Connection = require('oracledb/lib/connection');
+// const db_con = require('./oracle-conn.js')
+
+
+// const conn = db_con.establish_conn();
 
 con_str = "localhost/XEPDB1"
+const selectAllUsers = () => {
 
-oracledb.getConnection(
-    {
-        user:'sys',
-        password:'1025',
-        privilege: oracledb.SYSDBA,
-        tns:con_str
-    },
-    (err, con) => {
-        if(err){
-            console.log(err);
-            // res.send('db con error');
-            console.log("Connection error!");
-        }
-        else{
+    return new Promise((resolve, reject) => {
+
+        
+        // console.log(conn)
+        // const query = "SELECT * FROM \"_USER\""
+        // conn.execute(query, {}, { autoCommit: true })
+        //     .then((result) => {
+        //         return resolve(result.rows)
+        //     }).catch((err) => {
+        //         return reject(err)
+        //     })
+        oracledb.getConnection({
+            user: 'SYS',
+            password: '1025',
+            privilege: oracledb.SYSDBA,
+            tns: con_str
+        }).then((conn) => {
+
             const query = "SELECT * FROM \"_USER\""
-            con.execute(query,{},{autoCommit:true},function(e,s){
-                if(e){
-                    // res.send(e);
-                    console.log(e);
-                    console.log("Query error!")
-                }
-                else{
-                    console.log(s);
-                    // res.send(s);
-                    console.log("Query success!")
-                }
-            });
-        }
-    }
-);
+            conn.execute(query, {}, { autoCommit: true })
+                .then((result) => {
+                    return resolve(result.rows)
+                }).catch((err) => {
+                    console.log(err)
+                    return reject(err)
+                })
+
+        }).catch((err) => {
+            console.log(err);
+            return reject(err)
+        });
+
+    });
+
+};
+
+
+
+const checkUserPassword = (username, password) => {
+
+    return new Promise((resolve, reject) => {
+
+        
+        // console.log(conn)
+        // const query = "SELECT * FROM \"_USER\""
+        // conn.execute(query, {}, { autoCommit: true })
+        //     .then((result) => {
+        //         return resolve(result.rows)
+        //     }).catch((err) => {
+        //         return reject(err)
+        //     })
+        oracledb.getConnection({
+            user: 'SYS',
+            password: '1025',
+            privilege: oracledb.SYSDBA,
+            tns: con_str
+        }).then((conn) => {
+            
+            const query = `SELECT * FROM \"_USER\" WHERE USERNAME = '${username}' AND PASSWORD = '${password}'`
+            console.log(query)
+            conn.execute(query, {}, { autoCommit: true })
+                .then((result) => {
+                    // return resolve(result.rows)
+                    console.log(result.rows)
+                    return resolve(result.rows)
+                }).catch((err) => {
+                    console.log(err)
+                    return reject(err)
+                })
+
+        }).catch((err) => {
+            console.log(err);
+            return reject(err)
+        });
+
+    });
+
+};
+
+
+module.exports = {
+    selectAllUsers,
+    checkUserPassword
+}
